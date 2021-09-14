72ea055ac046ff8ddd0bcc820d6b534ae4d2e098
Rui Echizenya <rui.echizenya@gmail.com>
2021-09-14 19:07:52 +0900

    サーバ再起動監視用のshell

---
+++ ping_until_down.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# usage: ping_until_down.sh $target $timeout
+
+declare -r MAX=${2:-300}
+declare -i counter=$MAX+1
+declare -r PING="ping -c 1 -i 1 -W 1"
+declare -r TARGET=${1:-localhost}
+
+while [[ ! $((--counter)) -eq 0 ]]
+do
+    ${PING} ${TARGET} || break
+    echo $counter
+    sleep 1
+done >& /dev/null
+
+if [[ $counter -eq 0 ]] || [[ $counter -eq ${MAX} ]]
+then
+    exit 8
+else
+    exit 0
+fi
